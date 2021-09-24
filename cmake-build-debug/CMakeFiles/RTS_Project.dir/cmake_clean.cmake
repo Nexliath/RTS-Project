0d086379313a0355707e79c3367afc437806225e
Victor <victor.garnier@efrei.net>
2021-09-24 16:02:53 +0200

    Beginning

---
+++ cmake-build-debug/CMakeFiles/RTS_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTS_Project.dir/main.c.o"
+  "RTS_Project"
+  "RTS_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RTS_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
